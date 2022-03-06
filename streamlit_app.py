767ae4e9ff1833ed8e801d1e4f9200265b8d4a6e
MondeSinxi <monde.sinxi@gmail.com>
2022-03-06 15:07:31 +0200

    REF: standardise dataframes

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,6 +15,10 @@ PROVINCE_MAPPING = {"Eastern Cape": "EC", "Free State": "FS", "Gauteng": "GP",
                     "Mpumalanga": "MP", "Northern Cape": "NC", "North West": "NW",
                     "Western Cape": "WC", "Total": "total"}
 
+PROVINCE_CODE_MAPPING = {"EC": "Eastern Cape", "FS": "Free State", "GP": "Gauteng",
+                    "KZN": "KwaZulu-Natal", "LP": "Limpopo",
+                    "MP": "Mpumalanga", "NC": "Northern Cape", "NW": "North West",
+                    "WC": "Western Cape", "total": "Total"}
 
 def extract_data(data_file_path, date_column_name="date", date_format="%d-%m-%Y"):
     """ Read csv file and return dataframe"""
@@ -33,32 +37,52 @@ def plot_two_charts(data_1, data_2):
     layered_chart = alt.layer(graph_1, graph_2)
     return st.altair_chart(layered_chart, use_container_width=True)
 
+
+def format_name(s):
+    return s.replace('_', " ").capitalize()
+
 st.title('COVID-19 Dashboard')
 
 ########## Plot hospitalisation data ##################################
-st.write('Confirmed Cases and Hospitalisation Data')
+# st.write('Confirmed Cases and Hospitalisation Data')
 
 df_hosp = extract_data(HOSPITALISATION_DATA, date_format="%Y-%m-%d")
-df_cases = extract_data(CASES_PATH)
+df_cases_1 = extract_data(CASES_PATH)
+df_cases = df_cases_1[['EC', 'FS', 'GP', 'KZN', 'LP', 'MP', 'NC', 'NW', 'WC', 'total']].diff()
+df_cases["date"] = df_cases_1["date"]
 
+df_deaths_1 = extract_data(DEATHS_PATH)
+df_deaths = df_deaths_1[['EC', 'FS', 'GP', 'KZN', 'LP', 'MP', 'NC', 'NW', 'WC', 'total']].diff()
+df_deaths["date"] = df_deaths_1["date"]
+
+df_cases['type'] = 'cases'
+df_deaths['type'] = 'deaths'
+
+df = pd.concat([df_cases,df_deaths])
+
+df = df.melt(id_vars=['date', 'type'], value_vars=['total', 'EC', 'FS',  'GP', 'KZN', 'LP',  'MP', 'NC', 'NW', 'WC'])
+
+df["variable"] = df.variable.apply(lambda x: PROVINCE_CODE_MAPPING[x])
+df_hosp_melt = df_hosp.melt(id_vars=['date', 'province'], value_vars=['currently_admitted', 'current_in_icu'])
+df_hosp_melt.columns = ['date', 'variable', 'type', 'value']
+
+data = pd.concat([df,df_hosp_melt])
 # select a province
-selection = st.selectbox("Select Province", df_hosp.province.unique(),
+province_selection = st.selectbox("Select Province", data.variable.unique(),
                          index=0, help="select national or provincial data")
 
-province_code = PROVINCE_MAPPING[selection]
 
-# filter for current selection
-df_hosp_selection = df_hosp[df_hosp["province"] == selection]
+type_selection = st.multiselect("Select data type", data.type.unique(), default=None,
+                           format_func = format_name,
+                           help="select type of data to display")
 
-df_cases_selection = df_cases[["date", province_code]]
+# filter
+filtered_data = data[(data["type"].isin(type_selection)) & (data["variable"] == province_selection)]
 
-# get daily cases
-df_cases_selection["cases"] = df_cases_selection[[province_code]].diff()
+chart = alt.Chart(filtered_data).mark_area(opacity=0.3).encode(
+    x="date:T",
+    y=alt.Y("value:Q", stack=None),
+    color="type:N"
+).interactive()
 
-data_1 = {'data': df_hosp_selection, 'X': 'date:T', 'Y':  'currently_admitted'}
-data_2 = {'data': df_cases_selection, 'X': 'date:T', 'Y': 'cases'}
-
-plot_two_charts(SimpleNamespace(**data_1), SimpleNamespace(**data_2))
-
-
-#########################################################################
+st.altair_chart(chart, use_container_width=True)
