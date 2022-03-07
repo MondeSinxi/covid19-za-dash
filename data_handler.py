93740ab7c6d7a4b034f63fbbab237e5013b612e5
MondeSinxi <monde.sinxi@gmail.com>
2022-03-07 09:01:59 +0200

    REF: clean up data_handler code

--- data_handler.py
+++ data_handler.py
@@ -6,6 +6,7 @@ DATA_PATH = Path("covid19za/data/")
 HOSPITALISATION_DATA = Path('nicd_hospitalisation.csv')
 
 DEATHS_PATH = DATA_PATH / "covid19za_provincial_cumulative_timeline_deaths.csv"
+
 CASES_PATH = DATA_PATH / "covid19za_provincial_cumulative_timeline_confirmed.csv"
 
 PROVINCE_MAPPING = {"Eastern Cape": "EC", "Free State": "FS", "Gauteng": "GP",
@@ -24,23 +25,27 @@ def extract_data(data_file_path, date_column_name="date", date_format="%d-%m-%Y"
     df[date_column_name] = pd.to_datetime(df[date_column_name], format=date_format)
     return df.sort_values(by=date_column_name)
 
+def make_covid19za_data_long(data_path, type=None):
+    df_1 = extract_data(data_path)
+    df = df_1[['EC', 'FS', 'GP', 'KZN', 'LP', 'MP', 'NC', 'NW', 'WC', 'total']].diff()
+    df["date"] = df_1["date"]
+    df['type'] = type
+    return df
+
+def concat_long_covid19_za_data(data_frames: list[pd.DataFrame]):
+    df = pd.concat(data_frames)
+    df = df.melt(id_vars=['date', 'type'], value_vars=['total', 'EC', 'FS',  'GP', 'KZN', 'LP',  'MP', 'NC', 'NW', 'WC'])
+    df["variable"] = df.variable.apply(lambda x: PROVINCE_CODE_MAPPING[x])
+    return df
+
 df_hosp = extract_data(HOSPITALISATION_DATA, date_format="%Y-%m-%d")
-df_cases_1 = extract_data(CASES_PATH)
-df_cases = df_cases_1[['EC', 'FS', 'GP', 'KZN', 'LP', 'MP', 'NC', 'NW', 'WC', 'total']].diff()
-df_cases["date"] = df_cases_1["date"]
 
-df_deaths_1 = extract_data(DEATHS_PATH)
-df_deaths = df_deaths_1[['EC', 'FS', 'GP', 'KZN', 'LP', 'MP', 'NC', 'NW', 'WC', 'total']].diff()
-df_deaths["date"] = df_deaths_1["date"]
+df_cases = make_covid19za_data_long(CASES_PATH, type='cases')
 
-df_cases['type'] = 'cases'
-df_deaths['type'] = 'deaths'
+df_deaths = make_covid19za_data_long(DEATHS_PATH, type='deaths')
 
-df = pd.concat([df_cases,df_deaths])
+df = concat_long_covid19_za_data([df_cases, df_deaths])
 
-df = df.melt(id_vars=['date', 'type'], value_vars=['total', 'EC', 'FS',  'GP', 'KZN', 'LP',  'MP', 'NC', 'NW', 'WC'])
-
-df["variable"] = df.variable.apply(lambda x: PROVINCE_CODE_MAPPING[x])
 df_hosp_melt = df_hosp.melt(id_vars=['date', 'province'], value_vars=['currently_admitted', 'current_in_icu'])
 df_hosp_melt.columns = ['date', 'variable', 'type', 'value']
 
