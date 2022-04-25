b0d774b4b313e1b57b416dd27a0b1b94f0bf600f
MondeSinxi <monde.sinxi@gmail.com>
2022-04-25 08:39:26 +0200

    FMT: formatting with black

--- data_handler.py
+++ data_handler.py
@@ -3,73 +3,94 @@ from pathlib import Path
 
 DATA_PATH = Path("covid19za/data/")
 
-HOSPITALISATION_DATA = Path('nicd_hospitalisation.csv')
+HOSPITALISATION_DATA = Path("nicd_hospitalisation.csv")
 
 DEATHS_PATH = DATA_PATH / "covid19za_provincial_cumulative_timeline_deaths.csv"
 
 CASES_PATH = DATA_PATH / "covid19za_provincial_cumulative_timeline_confirmed.csv"
 
-PROVINCE_MAPPING = {"Eastern Cape": "EC", "Free State": "FS", "Gauteng": "GP",
-                    "KwaZulu-Natal": "KZN", "Limpopo": "LP",
-                    "Mpumalanga": "MP", "Northern Cape": "NC", "North West": "NW",
-                    "Western Cape": "WC", "Total": "total"}
+PROVINCE_MAPPING = {
+    "Eastern Cape": "EC",
+    "Free State": "FS",
+    "Gauteng": "GP",
+    "KwaZulu-Natal": "KZN",
+    "Limpopo": "LP",
+    "Mpumalanga": "MP",
+    "Northern Cape": "NC",
+    "North West": "NW",
+    "Western Cape": "WC",
+    "Total": "total",
+}
 
-PROVINCE_CODE_MAPPING = {"EC": "Eastern Cape", "FS": "Free State", "GP": "Gauteng",
-                         "KZN": "KwaZulu-Natal", "LP": "Limpopo",
-                         "MP": "Mpumalanga", "NC": "Northern Cape", "NW": "North West",
-                         "WC": "Western Cape", "total": "Total"}
+PROVINCE_CODE_MAPPING = {
+    "EC": "Eastern Cape",
+    "FS": "Free State",
+    "GP": "Gauteng",
+    "KZN": "KwaZulu-Natal",
+    "LP": "Limpopo",
+    "MP": "Mpumalanga",
+    "NC": "Northern Cape",
+    "NW": "North West",
+    "WC": "Western Cape",
+    "total": "Total",
+}
 
 PROVINCE_CODES = list(PROVINCE_CODE_MAPPING.keys())
 
 
 def extract_data(data_file_path, date_column_name="date", date_format="%d-%m-%Y"):
-    """ Read csv file and return dataframe"""
+    """Read csv file and return dataframe"""
     df = pd.read_csv(data_file_path)
-    df[date_column_name] = pd.to_datetime(
-        df[date_column_name], format=date_format)
+    df[date_column_name] = pd.to_datetime(df[date_column_name], format=date_format)
     return df.sort_values(by=date_column_name)
 
 
 def make_covid19za_data_long(data_path, type=None):
-    """" Convert data from wide to long form """
+    """ " Convert data from wide to long form"""
     df_extract = extract_data(data_path)
-    if 'daily' in type:
+    if "daily" in type:
         df = df_extract[PROVINCE_CODES].diff()
-    elif 'average' in type:
+    elif "average" in type:
         df = df_extract[PROVINCE_CODES].diff()
         df = df.rolling(7).mean().round(0)
     else:
         df = df_extract[PROVINCE_CODES]
     df["date"] = df_extract["date"]
-    df['type'] = type
+    df["type"] = type
     return df
 
 
 def concat_long_covid19_za_data(data_frames: list[pd.DataFrame]):
     df = pd.concat(data_frames)
-    df = df.melt(id_vars=['date', 'type'], value_vars=PROVINCE_CODES)
+    df = df.melt(id_vars=["date", "type"], value_vars=PROVINCE_CODES)
     df["variable"] = df.variable.apply(lambda x: PROVINCE_CODE_MAPPING[x])
     return df
 
 
 df_hosp = extract_data(HOSPITALISATION_DATA, date_format="%Y-%m-%d")
-df_hosp_melt = df_hosp.melt(id_vars=['date', 'province'], value_vars=[
-                            'currently_admitted', 'current_in_icu'])
-df_hosp_melt.columns = ['date', 'variable', 'type', 'value']
+df_hosp_melt = df_hosp.melt(
+    id_vars=["date", "province"], value_vars=["currently_admitted", "current_in_icu"]
+)
+df_hosp_melt.columns = ["date", "variable", "type", "value"]
 
-df_daily_cases = make_covid19za_data_long(CASES_PATH, type='daily_cases')
-df_cummulative_cases = make_covid19za_data_long(
-    CASES_PATH, type='cummulative_cases')
-df_average_cases = make_covid19za_data_long(CASES_PATH, type='average_cases')
+df_daily_cases = make_covid19za_data_long(CASES_PATH, type="daily_cases")
+df_cummulative_cases = make_covid19za_data_long(CASES_PATH, type="cummulative_cases")
+df_average_cases = make_covid19za_data_long(CASES_PATH, type="average_cases")
 
-df_daily_deaths = make_covid19za_data_long(DEATHS_PATH, type='daily_deaths')
-df_cummulative_deaths = make_covid19za_data_long(
-    DEATHS_PATH, type='cummulative_deaths')
-df_average_deaths = make_covid19za_data_long(
-    DEATHS_PATH, type='average_deaths')
+df_daily_deaths = make_covid19za_data_long(DEATHS_PATH, type="daily_deaths")
+df_cummulative_deaths = make_covid19za_data_long(DEATHS_PATH, type="cummulative_deaths")
+df_average_deaths = make_covid19za_data_long(DEATHS_PATH, type="average_deaths")
 
 
-df = concat_long_covid19_za_data([df_daily_cases, df_daily_deaths, df_cummulative_cases,
-                                 df_cummulative_deaths, df_average_cases, df_average_deaths])
+df = concat_long_covid19_za_data(
+    [
+        df_daily_cases,
+        df_daily_deaths,
+        df_cummulative_cases,
+        df_cummulative_deaths,
+        df_average_cases,
+        df_average_deaths,
+    ]
+)
 
 data = pd.concat([df, df_hosp_melt])
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,61 +1,79 @@
 import streamlit as st
 import pandas as pd
-import datetime
 import altair as alt
 
 from data_handler import data
 
+
 def extract_data(data_file_path, date_column_name="date", date_format="%d-%m-%Y"):
-    """ Read csv file and return dataframe"""
+    """Read csv file and return dataframe"""
     df = pd.read_csv(data_file_path)
     df[date_column_name] = pd.to_datetime(df[date_column_name], format=date_format)
     return df.sort_values(by=date_column_name)
 
-def format_name(s):
-    return s.replace('_', " ").capitalize()
 
-st.title('COVID-19 Dashboard')
+def format_name(s):
+    return s.replace("_", " ").capitalize()
+
+
+st.title("COVID-19 Dashboard")
 
 # select a province
-province_selection = st.selectbox("Select Province", data.variable.unique(),
-                         index=0, help="select national or provincial data")
+province_selection = st.selectbox(
+    "Select Province",
+    data.variable.unique(),
+    index=0,
+    help="select national or provincial data",
+)
 
 
-type_selection = st.multiselect("Select data type", data.type.unique(), default=None,
-                           format_func = format_name,
-                           help="select type of data to display")
-
+type_selection = st.multiselect(
+    "Select data type",
+    data.type.unique(),
+    default=None,
+    format_func=format_name,
+    help="select type of data to display",
+)
 
 # Filter
-filtered_data = data[(data["type"].isin(type_selection)) & (data["variable"] == province_selection)]
+filtered_data = data[
+    (data["type"].isin(type_selection)) & (data["variable"] == province_selection)
+]
 
 # Plot
-area_chart = alt.Chart(filtered_data).mark_area(opacity=0.3).encode(
-    x="date:T",
-    y=alt.Y("value:Q", stack=None),
-    color="type:N"
+area_chart = (
+    alt.Chart(filtered_data)
+    .mark_area(opacity=0.3)
+    .encode(x="date:T", y=alt.Y("value:Q", stack=None), color="type:N")
 )
 
 # Create a selection that chooses the nearest point & selects based on x-value
-nearest = alt.selection(type='single', nearest=True, on='mouseover',
-                        fields=['date'], empty='none')
+nearest = alt.selection(
+    type="single", nearest=True, on="mouseover", fields=["date"], empty="none"
+)
 
 
 # Transparent selectors across the chart. This is what tells us
 # the x-value of the cursor
-selectors = alt.Chart(filtered_data).mark_point().encode(
-    x='date:T',
-    opacity=alt.value(0),
-).add_selection(
-    nearest
+selectors = (
+    alt.Chart(filtered_data)
+    .mark_point()
+    .encode(
+        x="date:T",
+        opacity=alt.value(0),
+    )
+    .add_selection(nearest)
 )
 
 
 # Draw a rule at the location of the selection
-rules = alt.Chart(filtered_data).mark_rule(color='gray').encode(
-    x='date:T',
-).transform_filter(
-    nearest
+rules = (
+    alt.Chart(filtered_data)
+    .mark_rule(color="gray")
+    .encode(
+        x="date:T",
+    )
+    .transform_filter(nearest)
 )
 
 # Draw points on the line, and highlight based on selection
@@ -64,13 +82,15 @@ points = area_chart.mark_point().encode(
 )
 
 # Draw text labels near the points, and highlight based on selection
-text = area_chart.mark_text(align='left', dx=5, dy=-5).encode(
-    text=alt.condition(nearest, 'value:Q', alt.value(' '))
+text = area_chart.mark_text(align="left", dx=5, dy=-5).encode(
+    text=alt.condition(nearest, "value:Q", alt.value(" "))
 )
 
 # Put the five layers into a chart and bind the data
-layered_chart = alt.layer(
-    area_chart, selectors, points, rules, text
-).encode(tooltip=["date"]).interactive()
+layered_chart = (
+    alt.layer(area_chart, selectors, points, rules, text)
+    .encode(tooltip=["date"])
+    .interactive()
+)
 
 st.altair_chart(layered_chart, use_container_width=True)
